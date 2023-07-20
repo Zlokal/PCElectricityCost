bbdb9142f24ee85ecc83265ae7fd65b0df4423c0
Zlokal <135132873+Zlokal@users.noreply.github.com>
2023-07-20 20:30:03 +0200

    Update PCElectricityCost.py
    added new currency GBP

--- PCElectricityCost.py
+++ PCElectricityCost.py
@@ -8,8 +8,22 @@ daily_cost_usd = daily_cost_kwh*0.170
 month_cost_usd = month_cost_kwh*0.170
 
 print('Daily cost of using your computer is around', daily_cost_usd,'USD')
-month_question = input('Do u want to see what t cost monthly(yes or no)? ' )
+print('Power in kWh(per day): ', daily_cost_kwh)
+print('Power in kWh(per 30 days): ', month_cost_kwh)
+month_question = input('Do u want to see what it cost monthly(yes or no)? ' )
 if month_question == 'yes'.upper():
     print('Using your computer for 30 days for', device_working_time, 'hours a day will cost you about',month_cost_usd, 'USD')
 else:
     print('GoodBye!')
+elif currency == 'GBP':
+    daily_cost_usd = daily_cost_kwh*0.34
+    month_cost_usd = month_cost_kwh*0.34
+
+    print('Daily cost of using your computer is around', daily_cost_usd,'GBP')
+    print('Power in kWh(per day): ', daily_cost_kwh)
+    print('Power in kWh(per 30 days): ', month_cost_kwh)
+    month_question = input('Do u want to see what it cost monthly(yes or no)? ' )
+    if month_question == 'yes'.lower():
+        print('Using your computer for 30 days for', device_working_time, 'hours a day will cost you about',month_cost_usd, 'GBP')
+    else:
+        print('GoodBye!')
